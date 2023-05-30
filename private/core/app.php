5acffbdbf352927b2dfe6abd7f00c1f7ed0a442a
Tesar Rahmat Maulana <tesarrm586@gmail.com>
2023-05-30 10:41:23 +0700

    video ke 4
    Mengambil url di file App

--- private/core/app.php
+++ private/core/app.php
@@ -1,3 +1,20 @@
 <?php
 
-echo "from app.php";
+// main app file
+class App
+{
+	protected $controller = "home";
+  protected $method = "index";
+  protected $params = array();
+
+  public function __construct()
+  {
+    echo "<pre>";
+    print_r($this->getURL());
+  }
+  private function getURL()
+  {
+    return explode("/", filter_var(trim($_GET['url'], "/")),FILTER_SANITIZE_URL);
+  }
+}
+
--- public/index.php
+++ public/index.php
@@ -1,3 +1,5 @@
 <?php
 
 include("../private/core/autoload.php");
+
+$app = new App();
